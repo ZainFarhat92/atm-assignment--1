a0e6445f77ee70b6a73d715bc9ee7073469b856a
farhatzain <zain.farhat@programi.tech>
2025-06-17 14:16:25 +0300

    Deposit Money + Transaction Counter

--- atm.py
+++ atm.py
@@ -1,5 +1,6 @@
 balance = 1000  # Starting balance
 password = "0000" # Password used for login
+transactionCounter = 0  # Track the number of transactions deposits and withdrawl
 
 # Login to the ATM
 print("Welcome to the ATM!")
@@ -19,6 +20,19 @@ else:
         # Get the user's choice
         choice = input("Please enter your choice (1-4): ")
         
-        if choice == '1': # Check Balance
+        if choice == '1': #Check Balance
             print("Your current balance is: $" + str(balance))
+
+        elif choice == '2': # Deposit Money
+            try:
+                deposit = float(input("Enter the amount to deposit: "))
+                if deposit > 0:
+                    balance = balance + deposit
+                    print("$" + str(deposit) + " deposited successfully.")
+                    print( "Your New balance after deposit is: $" + str(balance))
+                    transactionCounter = transactionCounter + 1  # Increment transaction count
+                else:
+                    print("Deposit amount must be positive")
+            except ValueError:
+                print("Invalid input")
         
